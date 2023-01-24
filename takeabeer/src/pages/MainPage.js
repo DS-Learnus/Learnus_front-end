eb74345decaa857d1c9ab71ac915006f478fe87d
yxxyn20 <dbtndus0527@gmail.com>
2023-01-25 05:24:10 +0900

    메인페이지 디자인

--- takeabeer/src/pages/MainPage.js
+++ takeabeer/src/pages/MainPage.js
@@ -1,10 +1,69 @@
 import react from "react";
+import "../css/main-page.css";
 
 const MainPage = () => {
   return (
-    <>
-      <p>메인페이지입니다.</p>
-    </>
+    <div style={{ backgroundColor: "#faf7e7" }}>
+      <div className="introContainer">
+        <img src={require("../image/takeabeer-logo.png")} />
+        <div className="introTitle">
+          T A K E&nbsp;&nbsp; A&nbsp;&nbsp; B E E R
+        </div>
+        <div className="introContent">
+          우리 웹 페이지는 한국에서 유통되는 맥주를 소개하고 <br /> 사용자의
+          주량과 맞는 맥주를 추천해줍니다.
+          <br /> 또한, 여러 사용자들과 맥주 레시피를 공유하여 다양한 방법으로
+          맥주를 즐길 수 있습니다.
+        </div>
+      </div>
+      <div className="menuContainer">
+        <div className="menuTitle">M E N U</div>
+        <div className="menuBox">
+          <div style={{ fontFamily: "NanumSquareNeoRegular" }}>
+            <div className="menuBoxTitle">Let's Beer !</div>
+            <div className="menuBtn" style={{ backgroundColor: "#e2d1bd" }}>
+              <img
+                style={{ width: "250px", marginTop: "30px" }}
+                src={require("../image/beer-list.png")}
+              ></img>
+              <div className="menuBoxContent">
+                한국에서 유통하는 여러 가지 <br />
+                맥주에 대한 정보를 확인할 수 <br />
+                있습니다.
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="menuBoxTitle">Share Beer</div>
+            <div className="menuBtn" style={{ backgroundColor: "#f3ebbf" }}>
+              <img
+                style={{ width: "230px", marginTop: "80px" }}
+                src={require("../image/recipe-list.png")}
+              ></img>
+              <div className="menuBoxContent">
+                맥주를 사용한 레시피를 다른 <br />
+                사용자들과 나눔으로써 맥주를 <br />
+                다양하게 즐길 수 있습니다.
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="menuBoxTitle">Recommend</div>
+            <div className="menuBtn" style={{ backgroundColor: "#d4debd" }}>
+              <img
+                style={{ width: "190px", marginTop: "80px" }}
+                src={require("../image/recommend.png")}
+              ></img>
+              <div className="menuBoxContent">
+                사용자의 주량에 맞는 맥주와 <br />
+                해당 맥주를 사용한 <br />
+                주류 레시피를 추천해줍니다.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
