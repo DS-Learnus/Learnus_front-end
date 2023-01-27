cc37ba66d1a974ed6dac90f21d5b957da3523c10
yxxyn20 <dbtndus0527@gmail.com>
2023-01-27 21:51:01 +0900

    mypage 유저 정보 호출

--- takeabeer/src/pages/MyPageBeer.js
+++ takeabeer/src/pages/MyPageBeer.js
@@ -1,36 +1,54 @@
 import HeartItem from '../components/mypage/HeartItem';
-import '../css/MyPage/MyPageBeer.css'
-import { Container, Row, Col } from "react-bootstrap";
+import '../css/MyPage/MyPageBeer.css';
+import { Container, Row, Col } from 'react-bootstrap';
 import MyPageBtn from '../components/mypage/MyPageBtn';
 import { useSelector } from 'react-redux';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const MyPageBeer = () => {
+  const [likeBeerList, setLikeBeerList] = useState([]);
+  const [likeRecipeList, setLikeRecipeList] = useState([]);
 
-    const userId = useSelector(state => state.user.loginSuccess.user._id);
-    console.log(userId);
+  // userId 불러오기
+  const userId = useSelector((state) => state.user.loginSuccess.user._id);
+  console.log(userId);
 
-    const likeBeerList = useSelector(state => state.user.loginSuccess.user.likeBeers);
+  // 리스트 불러오기
+  useEffect(() => {
+    console.log('MyPageBeer');
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/api/user/mypage/${userId}`);
+        setLikeBeerList(response.data.mylikeBeer);
+        setLikeRecipeList(response.data.myLikeRecipe);
+        console.log(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
+  }, []);
 
-    return (
-        <div id='MyPageTemplate'>
-            <h1 className='MyPageTitle'>MY PAGE</h1>
-            <MyPageBtn/>
-            <div className='whiteBox'>
-                <Container>
-                    <Row>
-                        {likeBeerList.map((beerId) => {
-                            return(
-                                <Col md={3}>
-                                    <HeartItem beerId={beerId}/>
-                                </Col>
-                            )
-                        })}
-                    </Row>
-                </Container>
-            </div>
-        </div>
-    );
+  return (
+    <div id="MyPageTemplate">
+      <h1 className="MyPageTitle">MY PAGE</h1>
+      <MyPageBtn />
+      <div className="whiteBox">
+        <Container>
+          <Row>
+            {likeBeerList.map((beerId) => {
+              return (
+                <Col md={3}>
+                  <HeartItem beerId={beerId} />
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default MyPageBeer;
